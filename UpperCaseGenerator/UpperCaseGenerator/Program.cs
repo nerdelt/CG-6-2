3a7d51af5d36d7abb795a5faae16b95be8a1241e
nerdelt <nerdelt@gmail.com>
2018-08-16 17:20:14 -0500

    UpperCaseGenerator

--- UpperCaseGenerator/UpperCaseGenerator/Program.cs
+++ UpperCaseGenerator/UpperCaseGenerator/Program.cs
@@ -7,13 +7,17 @@ namespace UpperCaseGenerator
     {
         static void Main(string[] args)
         {
+            //Ask user for input
             Console.WriteLine("Welcome to the UPPER CASE GENERATOR!");
             Console.WriteLine("We take regular sentences and turn them into all UPPER CASE for you.\n");
             Console.Write("Please type in your sentence: ");
 
+            //converts user input to string
             string userSentence = Console.ReadLine();
+            //converts string to all upper case
             userSentence = userSentence.ToUpper();
 
+            //prints user input
             Console.WriteLine($"\nYour ugraded sentence: {userSentence}");
             Console.ReadLine();
 
